61f790d26a02c8ed51c6df4673f7f587a9b4e18f
Jordy <Jordyc59@nycstudents.net>
2025-01-13 09:01:14 -0500

    games
    using System.Collections;

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-
 public class PlayerController : MonoBehaviour
 {
   public InputAction moveActionz;
@@ -11,58 +9,53 @@ public class PlayerController : MonoBehaviour
   // Variables related to the health system
   public int maxHealth = 5;
   int currentHealth = 1;
-
   public int health { get { return currentHealth;}}
- 
    // Variables related to temporary Invincibility
     public float timeInvincible = 2.0f;
     bool isInvincible;
     float damageCooldown;
-
    // Start is called before the first frame update
    void Start()
    {     
       moveActionz.Enable();
+      currentHealth = maxHealth;
    }
    // Update is called once per frame
    void Update()
    {
-
-
      move = moveActionz.ReadValue<Vector2>();
-
      Debug.Log(move);
+      if (isInvincible)
+      {
+         damageCooldown -= Time.deltaTime;
+      
+         if (damageCooldown < 0)
+         {
+            isInvincible = false;
+            
+         }
+      }
    }
-     if (isInvincible)
-     {
-      damageCooldown -= Time.deltaTime;
-      if (damageCooldown < 0)
-   {
-       isInvincible = false;
-   }     
-   }
-
+     
    // FixedUpdate has the same call rate as the physics system
    void FixedUpdate()
    {
       Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
       transform.position = position;
-   }
-
-     
+   }  
    public void ChangeHealth(int amount)
    { 
       if (amount < 0)
       {
-         if (invincibility)
-      { 
-         return;
-      }   
-      IsInvincible = true;
-      damageCooldown = timeInvincible;
+         if (isInvincible)
+         { 
+            return;
+         }   
+         isInvincible = true;
+         damageCooldown = timeInvincible;
       }
       currentHealth = Mathf.Clamp(currentHealth = amount, 0, maxHealth);
       Debug.Log(currentHealth + "/" + maxHealth);
    }
-   
+
 } 
